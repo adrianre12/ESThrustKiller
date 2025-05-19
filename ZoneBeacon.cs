92138c340985099d7b00b68df30fcd90a1503247
adrianre12 <adrianrevill@hotmail.com>
2025-05-19 10:45:18 +0100

    Update ZoneBeacon.cs

--- ZoneBeacon.cs
+++ ZoneBeacon.cs
@@ -1,7 +1,11 @@
 ﻿using Sandbox.Common.ObjectBuilders;
 using Sandbox.Game.Entities;
+using Sandbox.Game.EntityComponents;
 using Sandbox.ModAPI;
+using System;
 using System.Collections.Generic;
+using System.Text;
+using VRage;
 using VRage.Game.Components;
 using VRage.Game.Entity;
 using VRage.ModAPI;
@@ -11,29 +15,25 @@ using VRageMath;
 namespace ESThrustKiller.ZoneBeacon
 {
 
-    public struct PositionInfo
-    {
-        public Vector3D Position;
-        public float Radius;
-
-        public PositionInfo(Vector3D position, float radius)
-        {
-            this.Position = position;
-            this.Radius = radius;
-        }
-    }
-
-
     [MyEntityComponentDescriptor(typeof(MyObjectBuilder_Beacon), false, "LargeBlockZoneBeacon")]
     public class ZoneBeaconGameLogic : MyGameLogicComponent
     {
-        private IMyBeacon myBeacon;
-        private bool debugLog = true;
+        const string VisualTexture = "SafeZone_Texture_Disco";
 
-        public static List<PositionInfo> Positions = new List<PositionInfo>();
-        private bool zoneCreated;
-        private MyEntity zoneId;
-        private MyEntity zoneId2;
+        public static Guid ZoneIdsKey = new Guid("0a1db65e-a169-4cf2-9a83-8903add9ca26");
+
+        private IMyBeacon myBeacon;
+
+        private MySafeZone zone1;
+        private MySafeZone zone2;
+
+        private float width = 10f;
+        private float height = 10f;
+        private float vertOffset = 2.5f;
+        private int numZones = 2;
+        private List<long> zoneIds = new List<long>();
+
+
 
         public override void Init(MyObjectBuilder_EntityBase objectBuilder)
         {
@@ -44,6 +44,9 @@ namespace ESThrustKiller.ZoneBeacon
             NeedsUpdate = MyEntityUpdateEnum.BEFORE_NEXT_FRAME;
 
             myBeacon = Entity as IMyBeacon;
+
+            if (myBeacon.Storage == null)
+                myBeacon.Storage = new MyModStorageComponent();
         }
 
         public override void UpdateOnceBeforeFrame()
@@ -52,8 +55,18 @@ namespace ESThrustKiller.ZoneBeacon
 
             if (myBeacon?.CubeGrid?.Physics == null) // ignore projected and other non-physical grids
                 return;
-            Log.Msg("Store position.");
-            Positions.Add(new PositionInfo(myBeacon.CubeGrid.GetPosition(), myBeacon.Radius));
+            string tmpIdsStr;
+            if (myBeacon.Storage.TryGetValue(ZoneIdsKey, out tmpIdsStr))
+            {
+                var tmp = tmpIdsStr.Split(',');
+                Log.Msg($"tmpIdsStr={tmpIdsStr} tmp.count={tmp.Length}");
+                foreach (var str in tmpIdsStr.Split(','))
+                {
+                    var val = Convert.ToInt64(str);
+                    Log.Msg($"{str}={val}");
+
+                }
+            }
             NeedsUpdate = MyEntityUpdateEnum.EACH_100TH_FRAME;
         }
 
@@ -61,48 +74,84 @@ namespace ESThrustKiller.ZoneBeacon
         {
             if (!MyAPIGateway.Session.IsServer)
                 return;
-            Log.Msg("Update100.");
+            //Log.Msg("Update100.");
 
-            CreateZone();
+            CreateZones();
 
-            //var tmp = zoneId as MySafeZone;
-            //Log.Msg(tmp.CurrentTexture.String);
         }
 
-        public void CreateZone()
+        public void CreateZones()
         {
-            if (zoneCreated)
+            if (zoneIds.Count > 0)
             {
                 return;
             }
-            zoneCreated = true;
+            zoneIds.Clear();
 
-            var colour = new Vector3D(255, 0, 0);
+            var colour = new Vector3D(0, 255, 0);
 
-            var position = myBeacon.GetPosition();
-            zoneId = MySessionComponentSafeZones.CrateSafeZone(MatrixD.CreateWorld(position), MySafeZoneShape.Sphere, MySafeZoneAccess.Blacklist, null, null, 10f, true, true, colour, "SafeZone_Texture_Disco", 0L, "Test1");
+            //var position1 = myBeacon.GetPosition() + myBeacon.WorldMatrix.Up * vertOffset;
+            //zone1 = (MySafeZone)MySessionComponentSafeZones.CrateSafeZone(MatrixD.CreateWorld(position1, myBeacon.WorldMatrix.Forward, myBeacon.WorldMatrix.Up), MySafeZoneShape.Box, MySafeZoneAccess.Blacklist, null, null, 10f, true, true, colour, "SafeZone_Texture_Disco", 0L, "FlyZone1");
+            //zone1.Size = new Vector3D(width, height, width);
+            //var position2 = position1 + myBeacon.WorldMatrix.Up * (height + 0.01);
+            //zoneId2 = MySessionComponentSafeZones.CrateSafeZone(MatrixD.CreateWorld(position2, myBeacon.WorldMatrix.Forward, myBeacon.WorldMatrix.Up), MySafeZoneShape.Box, MySafeZoneAccess.Blacklist, null, null, 10f, true, true, colour, "SafeZone_Texture_Disco", 0L, "FlyZone2");
+            // ((MySafeZone)zoneId2).Size = new Vector3D(width, height, width);
 
-            var position2 = position;
-            position2 = position + myBeacon.WorldMatrix.Up * 20.01;
-            zoneId2 = MySessionComponentSafeZones.CrateSafeZone(MatrixD.CreateWorld(position2), MySafeZoneShape.Sphere, MySafeZoneAccess.Blacklist, null, null, 10f, true, true, colour, "SafeZone_Texture_Disco", 0L, "Test2");
+            //ob.AllowedActions = CastHax(MySessionComponentSafeZones.AllowedActions, 0x3FF);
 
+            //zone1 = (MySafeZone)CrateSafeZone(MatrixD.CreateWorld(position1, myBeacon.WorldMatrix.Forward, myBeacon.WorldMatrix.Up), MySafeZoneShape.Box, colour, "FlyZone1");
 
-            Log.Msg($"Zone created: {zoneCreated}");
-
+            var zoneIdsStr = new StringBuilder();
+            var position = myBeacon.GetPosition() + myBeacon.WorldMatrix.Up * vertOffset;
+            for (int i = 0; i < numZones; i++)
+            {
+                position += i * myBeacon.WorldMatrix.Up * (height + 0.01);
+                var zone = (MySafeZone)CrateSafeZone(MatrixD.CreateWorld(position, myBeacon.WorldMatrix.Forward, myBeacon.WorldMatrix.Up), MySafeZoneShape.Box, colour, $"FlyZone{i + 1}");
+                zoneIds.Add(zone.EntityId);
+                zoneIdsStr.Append($"{zone.EntityId},");
+            }
+            ;
+            Log.Msg($"zoneIdsStr={zoneIdsStr.ToString()}");
+            myBeacon.Storage[ZoneIdsKey] = zoneIdsStr.ToString();
+            Log.Msg($"Entity.Storage[ZoneIdsKey] ={myBeacon.Storage[ZoneIdsKey]}");
         }
+
+        //public static T CastHax<T>(T typeRef, object castObj) => (T)castObj;
+
+        public MyEntity CrateSafeZone(MatrixD transform, MySafeZoneShape safeZoneShape, Vector3 colour = default(Vector3), string name = null)
+        {
+            MyObjectBuilder_SafeZone myObjectBuilder_SafeZone = new MyObjectBuilder_SafeZone();
+            myObjectBuilder_SafeZone.Size = new Vector3D(width, height, width);
+            myObjectBuilder_SafeZone.PositionAndOrientation = new MyPositionAndOrientation(transform);
+            myObjectBuilder_SafeZone.Radius = 10f;
+            myObjectBuilder_SafeZone.PersistentFlags = MyPersistentEntityFlags2.InScene;
+            myObjectBuilder_SafeZone.Shape = safeZoneShape;
+            myObjectBuilder_SafeZone.AccessTypePlayers = MySafeZoneAccess.Blacklist;
+            myObjectBuilder_SafeZone.AccessTypeFactions = MySafeZoneAccess.Blacklist; ;
+            myObjectBuilder_SafeZone.AccessTypeGrids = MySafeZoneAccess.Blacklist; ;
+            myObjectBuilder_SafeZone.AccessTypeFloatingObjects = MySafeZoneAccess.Blacklist;
+            myObjectBuilder_SafeZone.IsVisible = true;
+            myObjectBuilder_SafeZone.ModelColor = colour;
+            myObjectBuilder_SafeZone.Texture = VisualTexture;
+            myObjectBuilder_SafeZone.Enabled = true;
+            myObjectBuilder_SafeZone.SafeZoneBlockId = 0l;
+            myObjectBuilder_SafeZone.Name = (myObjectBuilder_SafeZone.DisplayName = name);
+            //myObjectBuilder_SafeZone.AllowedActions = CastHax(MySessionComponentSafeZones.AllowedActions, 0x3FF);
+            return MyEntities.CreateFromObjectBuilderAndAdd(myObjectBuilder_SafeZone, fadeIn: false);
+        }
+
 
         public override void OnRemovedFromScene()
         {
-            if (zoneId != null)
+            if (zoneIds.Count == 0)
             {
-
-                MySessionComponentSafeZones.RequestDeleteSafeZone(zoneId.EntityId);
-
+                return;
             }
-            if (zoneId2 != null)
+            foreach (var zoneId in zoneIds)
             {
-                MySessionComponentSafeZones.RequestDeleteSafeZone(zoneId2.EntityId);
+                MySessionComponentSafeZones.RequestDeleteSafeZone(zoneId);
             }
+
         }
     }
 }
